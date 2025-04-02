36e28157b57df54996ea36124133da3c473261fe
codeNow26 <code.now26@gmail.com>
2025-04-02 10:51:11 +0200

    added changeAmount() function and amount to json, added orderitem bar

--- script.js
+++ script.js
@@ -2,12 +2,21 @@
 
 function addDishToCart(i) {
     let contentRef = document.getElementById('cart');
-    contentRef.innerHTML += 
-    `
-    <h3 class="cart-dish-title">${Object.values(myDishes[i].name).join("")}</h3>
-     <p>${myDishes[i].price}0€</p>
+    contentRef.innerHTML +=
+        `
+    <h3 class="cart-dish-title">${myDishes[i].name}</h3>
+    <p>${myDishes[i].price}0€</p>
+    <div class="order-item">
+     <img class="change-amount-btn" src="./assets/icons/minus-kreisformiger-knopf.png" alt="Minus Button" class="cart-dish-remove">
+     <p>${myDishes[i].amount}</p>
+     <img onclick="changeAmount(i)" class="change-amount-btn" src="./assets/icons/plus.png">
+    </div>
      `
 }
+
+// check onclick bei plus
+
+
 
 function addPrice() {
 
--- scripts/database.js
+++ scripts/database.js
@@ -2,20 +2,37 @@ let myDishes = [
     {
         "name": "Spaghetti Carbonara",
         "price": 12.90,
+        "amount": 1,
         "description": "mit Schinken und Käse"
     },
     {
         "name": "Spaghetti Bolognese",
         "price": 13.90,
+        "amount": 1,
         "description": "Wasser"
     },
     {
       "name": "I hobs",
       "price": 2000.23,
+      "amount": 1,
       "description": "YESSSSS"
   }
 ]
 
- let dishy = myDishes.filter((element) => { return element['name'] == "Spaghetti Carbonara" });
+function changeAmount(i) {
+    change = myDishes[i].amount;
+    if (change === 0) {
+        return;
+    }
+    else {
+        for (let i = 1; i < 99; i++) {
+            const element = myDishes[i];
+            
+        }
+    }
+    return change;
 
- console.log(Object.values(myDishes[0]));
+}
+
+console.log(changeAmount(1))
+
