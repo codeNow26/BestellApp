18010f581e6a91f15c207c21590b86a97e6df26e
codeNow26 <code.now26@gmail.com>
2025-04-19 09:58:23 +0200

    made the Website responsive

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function addDishToCart(i) {
     if (cartItem) {
         if (cartItem.style.display === 'none') {
             showCartItem(i);
+            addPrice(i, 'plus');
         } else {
             changeAmount(i, 'plus');
+            addPrice(i, 'plus');
         }
     } else {
         createCartItem(i);
@@ -14,22 +16,64 @@ function addDishToCart(i) {
 function showCartItem(i) {
     let cartItem = document.getElementById(`cart-wrapper-${i}`);
     cartItem.style.display = 'block';
-    document.getElementById(`amount-counter-${i}`).innerText = 1;
 }
 
 function createCartItem(i) {
-    let contentRef = document.getElementById('cart');
-    contentRef.innerHTML += `
-        <div id="cart-wrapper-${i}" class="cart-wrapper">
-            <h3 class="cart-dish-title">${myDishes[i].name}</h3>
-            <p id="price-counter-${i}">${myDishes[i].price}0€</p>
-            <div class="order-item">
-                <img onclick="changeAmount(${i}, 'minus')" class="change-amount-btn" src="./assets/icons/minus-kreisformiger-knopf.png" alt="Minus Button">
-                <p id="amount-counter-${i}">${myDishes[i].amount}</p>
-                <img onclick="changeAmount(${i}, 'plus'), addPriceToTotal(${i})" class="change-amount-btn" src="./assets/icons/plus.png">
-            </div>
-        </div>
-    `;
+    let cartItemHTML = generateCartItemHTML(i);
+    addCartItemToCart(cartItemHTML);
     showCartItem(i);
 }
 
+function generateCartItemHTML(i) {
+    return `
+        <div id="cart-wrapper-${i}" class="cart-wrapper">
+          <div class=cart-title-trash><h3 class="cart-dish-title">${myDishes[i].name}</h3>
+            <img onclick="addDishToTrash(${i})" class="trash-bin"  src="assets/icons/trash-bin.png"></div>
+            <p id="price-counter-${i}">${myDishes[i].price.toFixed(2)}€</p>
+            <div class="order-item">
+                <img onclick="changeAmount(${i}, 'minus'), addPrice(${i}, 'minus'), addPriceToTotal()" class="change-amount-btn" src="./assets/icons/minus-kreisformiger-knopf.png" alt="Minus Button">
+                <p id="amount-counter-${i}">${myDishes[i].amount}</p>
+                <img onclick="changeAmount(${i}, 'plus'), addPrice(${i}, 'plus'), addPriceToTotal()" class="change-amount-btn" src="./assets/icons/plus.png">
+                </div>
+        </div>
+    `;
+}
+
+function addCartItemToCart(html) {
+    let contentRef = document.getElementById('cart');
+    contentRef.innerHTML += html;
+}
+
+
+function order() {
+    orderButton = document.getElementById('order-button');
+    orderButtonText = document.getElementById('order-button-text');
+    let cart = document.getElementsByClassName('cart-wrapper');
+    for (let i = 0; i < cart.length; i++) {
+        addDishToTrash(i);
+    }
+    orderButton.style.display = "none";
+    orderButtonText.style.display = "block";
+}
+
+function generateOrder() {
+    orderButtonText = document.getElementById('order-button-text');
+    orderButton = document.getElementById('order-button');
+    orderButton.innerHTML = `
+    <button onclick="order()" class="order-btn">Bezahlen</button>`;
+    if (orderButton.style.display === 'none') {
+        orderButton.style.display = 'block';
+    }
+    orderButtonText.style.display = 'none';
+}
+
+function DishClick(i) {
+    addDishToCart(i);
+    generateOrder();
+    addPriceToTotal();
+}
+
+function showOverlay() {
+  document.getElementById('shopping-cart').style.display = "block";
+  document.getElementById('menu-wrapper').style.display = "none";
+}
--- scripts/database.js
+++ scripts/database.js
@@ -1,35 +1,76 @@
 let myDishes = [
     {
-        "name": "Spaghetti Carbonara",
+        "name": "Bibimbap",
         "price": 12.90,
         "value": 12.90,
         "amount": 1,
-        "description": "mit Schinken und Käse"
+        "description": "Reis mit Gemüse, Ei, Fleisch, scharfer Sauce (Gochujang)"
     },
     {
-        "name": "Spaghetti Bolognese",
+        "name": "Dakgalbi",
         "price": 13.90,
         "value": 13.90,
         "amount": 1,
         "description": "Wasser"
     },
     {
-        "name": "Spaghetti alio",
-        "price": 2000.23,
-        "value": 2000.23,
+        "name": "Bulgogi",
+        "price": 14.90,
+        "value": 14.90,
         "amount": 1,
         "description": "YESSSSS"
-    }
+    },
+    {
+        "name": "Tteokbokki",
+        "price": 4.90,
+        "value": 4.90,
+        "amount": 1,
+        "description": "was los"
+    },
+    {
+        "name": "Mandu",
+        "price": 22.90,
+        "value": 22.90,
+        "amount": 1,
+        "description": "was los"
+    },
+    {
+        "name": "Eomuk",
+        "price": 5.90,
+        "value": 5.90,
+        "amount": 1,
+        "description": "was los"
+    },
+    {
+        "name": "Kimchi",
+        "price": 3.90,
+        "value": 3.90,
+        "amount": 1,
+        "description": "was los"
+    },
+    {
+        "name": "Oi Muchim",
+        "price": 3.90,
+        "value": 3.90,
+        "amount": 1,
+        "description": "was los"
+    },
+    {
+        "name": "Gamja Jorim",
+        "price": 4.90,
+        "value": 4.90,
+        "amount": 1,
+        "description": "was los"
+    },
 ]
 
 function changeAmount(i, operation) {
     let element = document.getElementById(`amount-counter-${i}`);
     let cart = document.getElementById(`cart-wrapper-${i}`);
-    let amount = parseInt(element.innerText);
+    let amount = parseInt(element.innerText);;
     if (operation == 'plus') {
-        element.innerText = amount + 1;
+        element.innerText = amount + 1;  
     }
-
     if (operation == 'minus') {
         if (amount === 1) {
             cart.style.display = "none";
@@ -39,11 +80,50 @@ function changeAmount(i, operation) {
     }
 }
 
-function addPriceToTotal(i) {
+function addPrice(i, operation) {
     let price = document.getElementById(`price-counter-${i}`);
-    myDishes[i].price += myDishes[i].value;
-    myDishes[i].price = parseFloat(myDishes[i].price.toFixed(2));
-    price.innerText = myDishes[i].price + "0€";
+    if (operation == 'plus') {
+        myDishes[i].price += myDishes[i].value;
+        myDishes[i].price = parseFloat(myDishes[i].price.toFixed(2));
+        price.innerText = myDishes[i].price.toFixed(2) + "€";
+        return;
+    }
+    if (operation == 'minus') {
+        myDishes[i].price -= myDishes[i].value
+        price.innerText = myDishes[i].price.toFixed(2) + "€";
+        return;
+    }
 }
 
-// gleitkommastelle beheben
+function addDishToTrash(i) {
+    let cartItem = document.getElementById(`cart-wrapper-${i}`);
+    let element = document.getElementById(`amount-counter-${i}`);
+    let priceView = document.getElementById('total');
+    if (  myDishes[i].price = 0) {
+        priceView.style.display = 'none';
+    }
+    cartItem.style.display = 'none';
+    myDishes[i].price = 0;
+    element.innerText = 1;
+    addPriceToTotal();
+}
+
+function addPriceToTotal() {
+
+    let price = document.getElementById('total');
+    let cart = document.getElementsByClassName('cart-wrapper');
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].style.display !== 'none') {
+            total += myDishes[i].price;
+        }
+    }
+    total = Math.round(total * 100) / 100;
+    if (total > 0) {
+        price.style.display = "block";
+    } else {
+        price.style.display = "none";
+    }
+    price.innerText = "Total: " + total.toFixed(2) + "€";
+    addPrice();
+}
